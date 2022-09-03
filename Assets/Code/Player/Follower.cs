d637934f9d657392c8457dd73746b33c0bbc7f90
Rapuška <megarubka44@gmail.com>
2022-09-04 00:07:24 +0300

    Fixed inversion on/off modes

--- Assets/Code/Player/Follower.cs
+++ Assets/Code/Player/Follower.cs
@@ -1,6 +1,6 @@
-using System;
 using Mirror;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Code.Player
 {
@@ -8,25 +8,25 @@ namespace Code.Player
 	{
 		[SerializeField] private float _mouseSensitivity;
 		[SerializeField] private float _distanceFromTarget;
-		[SerializeField] private float _smoothTime;
-		[SerializeField] private Vector2 _rotationXMinMax;
+		[FormerlySerializedAs("_smoothTime")] [SerializeField] private float _smooth;
+		[SerializeField] private float _rotationXMin;
+		[SerializeField] private float _rotationXMax;
 		[SerializeField] private Transform _target;
 		[SerializeField] private NetworkBehaviour _networkBehaviour;
-		
+		[SerializeField] private bool _invertCamera;
+
 		private float _rotationY;
 		private float _rotationX;
 		private Vector3 _currentRotation;
 		private Vector3 _smoothVelocity = Vector3.zero;
 
-		private void Start()
-		{
-			gameObject.SetActive(_networkBehaviour.hasAuthority);
-		}
+		private float ActualRotationX => _invertCamera
+			? _rotationX
+			: -_rotationX;
 
-		private void Update()
-		{
-			RotateCamera();
-		}
+		private void Start() => gameObject.SetActive(_networkBehaviour.hasAuthority);
+
+		private void Update() => RotateCamera();
 
 		private void RotateCamera()
 		{
@@ -36,17 +36,31 @@ namespace Code.Player
 			_rotationY += mouseX;
 			_rotationX += mouseY;
 
-			// Apply clamping for x rotation 
-			_rotationX = Mathf.Clamp(_rotationX, _rotationXMinMax.x, _rotationXMinMax.y);
+			ApplyClampingByX();
 
-			var nextRotation = new Vector3(_rotationX, _rotationY);
+			var nextRotation = new Vector3(ActualRotationX, _rotationY);
 
-			// Apply damping between rotation changes
-			_currentRotation = Vector3.SmoothDamp(_currentRotation, nextRotation, ref _smoothVelocity, _smoothTime);
-			transform.localEulerAngles = _currentRotation;
+			ApplyDampingBetweenRotations(nextRotation);
 
-			// Substract forward vector of the GameObject to point its forward vector to the target
+			SubstractForwardToPointForwardToTarget();
+		}
+
+		private void SubstractForwardToPointForwardToTarget()
+		{
 			transform.position = _target.position - transform.forward * _distanceFromTarget;
+		}
+
+		private void ApplyDampingBetweenRotations(Vector3 nextRotation)
+		{
+			_currentRotation = Vector3.SmoothDamp(_currentRotation, nextRotation, ref _smoothVelocity, _smooth);
+			transform.localEulerAngles = _currentRotation;
+		}
+
+		private void ApplyClampingByX()
+		{
+			_rotationX = _invertCamera
+				? Mathf.Clamp(_rotationX, _rotationXMax, _rotationXMin)
+				: Mathf.Clamp(_rotationX, -_rotationXMin, -_rotationXMax);
 		}
 	}
 }
