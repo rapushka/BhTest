3ee761ea7a0291013badab30b76bbb7513c5e95c
Rapuška <megarubka44@gmail.com>
2022-09-04 09:52:55 +0300

    Rotating without mesh

--- Assets/Code/Player/PlayerMovement.cs
+++ Assets/Code/Player/PlayerMovement.cs
@@ -8,6 +8,7 @@ namespace Code.Player
 		[SerializeField] private InputEmit _input;
 		[SerializeField] private float _movementSpeed;
 		[SerializeField] private CharacterController _characterController;
+		[SerializeField] private Transform _followCamera;
 
 		private float ScaledSpeed => _movementSpeed * Time.fixedDeltaTime;
 
@@ -16,6 +17,7 @@ namespace Code.Player
 		private void Move()
 			=> _input.MoveDirection
 			         .ToXZ()
+			         .Set((d) => Quaternion.Euler(0, _followCamera.eulerAngles.y, 0) * d)
 			         .Set((d) => d * ScaledSpeed)
 			         .Do(_characterController.Move, @if: (d) => d != Vector3.zero);
 	}
