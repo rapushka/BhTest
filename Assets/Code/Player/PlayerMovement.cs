8b909778704f80bea35f0f4cfb9b8587acc4165e
Rapuška <megarubka44@gmail.com>
2022-09-04 09:54:37 +0300

    Minor Refactoring

--- Assets/Code/Player/PlayerMovement.cs
+++ Assets/Code/Player/PlayerMovement.cs
@@ -10,15 +10,18 @@ namespace Code.Player
 		[SerializeField] private CharacterController _characterController;
 		[SerializeField] private Transform _followCamera;
 
-		private float ScaledSpeed => _movementSpeed * Time.fixedDeltaTime;
-
 		private void FixedUpdate() => Move();
 
 		private void Move()
 			=> _input.MoveDirection
 			         .ToXZ()
-			         .Set((d) => Quaternion.Euler(0, _followCamera.eulerAngles.y, 0) * d)
-			         .Set((d) => d * ScaledSpeed)
+			         .Set(FacingByCamera)
+			         .Set(Scale)
 			         .Do(_characterController.Move, @if: (d) => d != Vector3.zero);
+
+		private Vector3 Scale(Vector3 d) => d * (_movementSpeed * Time.fixedDeltaTime);
+
+		private Vector3 FacingByCamera(Vector3 direction) 
+			=> Quaternion.Euler(0, _followCamera.eulerAngles.y, 0) * direction;
 	}
 }
