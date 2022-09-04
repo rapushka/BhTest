7c4c759f7ba4d2d574a6f05c22cb337cdd3e405d
Rapuška <megarubka44@gmail.com>
2022-09-04 14:29:16 +0300

    Minor Refactoring

--- Assets/Code/Player/MaterialChangeComponent.cs
+++ Assets/Code/Player/MaterialChangeComponent.cs
@@ -10,34 +10,15 @@ namespace Code.Player
 		[SerializeField] private Color _changed;
 
 		[SyncVar(hook = nameof(SyncColor))] private Color _color;
-		
-		[Command]
-		private void CmdApplyColor(Color material)
+
+		public void ToDefaultColor()
 		{
-			ApplyColorOnServer(material);
-		}
-		
-		[Server]
-		private void ApplyColorOnServer(Color color)
-		{
-			_color = color;
+			ChangeColor(_default);
 		}
 
-		private void Update()
+		public void ToChangedColor()
 		{
-			if (hasAuthority == false)
-			{
-				return;
-			}
-
-			if (Input.GetKeyDown(KeyCode.Mouse1))
-			{
-				ChangeColor(_changed);
-			}
-			if (Input.GetKeyDown(KeyCode.Mouse2))
-			{
-				CmdApplyColor(_default);
-			}
+			ChangeColor(_changed);
 		}
 
 		private void ChangeColor(Color color)
@@ -51,6 +32,10 @@ namespace Code.Player
 				CmdApplyColor(color);
 			}
 		}
+
+		[Command] private void CmdApplyColor(Color material) => ApplyColorOnServer(material);
+
+		[Server] private void ApplyColorOnServer(Color color) => _color = color;
 
 		private void SyncColor(Color oldValue, Color newValue) => _renderer.material.color = newValue;
 	}
