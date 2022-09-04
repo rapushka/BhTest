2079b5eeb8970e26a182f6e1190a77f515e6134a
Rapuška <megarubka44@gmail.com>
2022-09-04 10:07:46 +0300

    Correct rotating

--- Assets/Code/Player/CharacterRotateToMoveDirection.cs
+++ Assets/Code/Player/CharacterRotateToMoveDirection.cs
@@ -1,26 +1,25 @@
-using Code.Workflow.Extensions;
 using UnityEngine;
 
 namespace Code.Player
 {
-	public class PlayerToMoveDirectionRotator : MonoBehaviour
+	public class CharacterRotateToMoveDirection : MonoBehaviour
 	{
-		[SerializeField] private InputEmit _input;
 		[SerializeField] private Transform _transform;
 		[SerializeField] private float _rotationSpeed;
-
+		[SerializeField] private CharacterController _characterController;
+		
 		private float ScaledSpeed => _rotationSpeed * Time.deltaTime;
 		
 		private void Update() => Rotate();
 
 		private void Rotate()
 		{
-			if (_input.MoveDirection == Vector2.zero)
+			if (_characterController.velocity == Vector3.zero)
 			{
 				return;
 			}
 
-			Quaternion desiredRotation = Quaternion.LookRotation(_input.MoveDirection.ToXZ(), Vector3.up);
+			Quaternion desiredRotation = Quaternion.LookRotation(_characterController.velocity, Vector3.up);
 			_transform.rotation = Quaternion.Slerp(_transform.rotation, desiredRotation, ScaledSpeed);
 		}
 	}
