ea1abd5a8c36b1fb0d02e71791a689348fa713b3
Rapuška <megarubka44@gmail.com>
2022-09-04 09:22:32 +0300

    Split Initialization and Rotaition Code

--- Assets/Code/Player/InputEmit.cs
+++ Assets/Code/Player/InputEmit.cs
@@ -6,6 +6,7 @@ namespace Code.Player
 	public class InputEmit : NetworkBehaviour
 	{
 		public Vector2 MoveDirection { get; private set; }
+		public Vector2 MouseRotation { get; private set; }
 
 		private void Update()
 		{
@@ -13,16 +14,26 @@ namespace Code.Player
 			{
 				return;
 			}
-			
-			Movement();
+
+			SetMoveDirection();
+			SetMouseRotation();
 		}
 
-		private void Movement()
+		private void SetMoveDirection()
 		{
 			MoveDirection = new Vector2
 			{
 				x = Input.GetAxis("Horizontal"),
 				y = Input.GetAxis("Vertical"),
+			};
+		}
+
+		private void SetMouseRotation()
+		{
+			MouseRotation = new Vector2
+			{
+				x = Input.GetAxis("Mouse X"),
+				y = Input.GetAxis("Mouse Y")
 			};
 		}
 	}
