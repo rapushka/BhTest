93cf944ebeb7eb3a61d9e970337bf1e50fc66640
Rapuška <megarubka44@gmail.com>
2022-09-05 22:37:02 +0300

    Sync score but not color

--- Assets/Code/Player/Score/PlayerScore.cs
+++ Assets/Code/Player/Score/PlayerScore.cs
@@ -9,17 +9,32 @@ namespace Code.Player.Score
 		[SerializeField] private Vector2 _scale;
 		[SerializeField] private float _spaceBetween;
 
-		[SyncVar] private int _scoreValue;
+		[SyncVar(hook = nameof(SyncScore))] private int _syncScoreValue;
 		[SyncVar] private int _index;
 		[SyncVar] private string _playerName;
+
+		private int _scoreValue;
 
 		public void Construct(string playerName, int index)
 		{
 			_playerName = playerName;
 			_index = index;
 		}
-		
-		public void IncrementScore() => _scoreValue++;
+
+		public void IncrementScore()
+		{
+			if (isServer)
+			{
+				ChangeScore(_syncScoreValue + 1);
+			}
+			else
+			{
+				CmdChangeScore(_syncScoreValue + 1);
+			}
+		}
+
+		[Command] private void CmdChangeScore(int newScore) => ChangeScore(newScore);
+		[Server] private void ChangeScore(int newScore) => _syncScoreValue = newScore;
 
 		private void OnGUI()
 		{
@@ -32,8 +47,10 @@ namespace Code.Player.Score
 					width: _scale.x,
 					height: _scale.y
 				),
-				$"{_playerName}: {_scoreValue}"
+				$"{_playerName}: {_syncScoreValue}"
 			);
 		}
+
+		private void SyncScore(int _, int newValue) => _scoreValue = newValue;
 	}
 }
