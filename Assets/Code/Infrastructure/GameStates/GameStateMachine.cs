97bb1cd4a783148b53cf0ce7351943e039129505
Rapuška <megarubka44@gmail.com>
2022-09-07 03:10:12 +0300

    Fixed

--- Assets/Code/Infrastructure/GameStates/GameStateMachine.cs
+++ Assets/Code/Infrastructure/GameStates/GameStateMachine.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using Code.Infrastructure.StateMachines;
-using Code.Player.Score;
 using UnityEngine;
 
 namespace Code.Infrastructure.GameStates
@@ -10,13 +9,8 @@ namespace Code.Infrastructure.GameStates
 	{
 		[SerializeField] private int _scoreToWin = 3;
 		[SerializeField] private float _secondsOnWinScreen = 5f;
-
-		private PlayerScore[] _playerScores;
 		
 		public string LastSavedPlayerName { get; private set; }
-
-		private IEnumerable<PlayerScore> PlayerScores
-			=> _playerScores ??= FindObjectsOfType<PlayerScore>();
 
 		protected override Dictionary<Type, IGameState> CreateStatesDictionary()
 			=> new Dictionary<Type, IGameState>
@@ -25,38 +19,14 @@ namespace Code.Infrastructure.GameStates
 				[typeof(GameplayState)] = new GameplayState(_scoreToWin),
 				[typeof(GameWinState)] = new GameWinState(_secondsOnWinScreen),
 			};
-
-		private void OnDisable()
-		{
-			foreach (PlayerScore playerScore in PlayerScores)
-			{
-				playerScore.ScoreIncrease -= OnScoreIncrease;
-			}
-		}
-
-		private void OnScoreIncrease(string playerName, int score)
+		
+		public void OnScoreIncrease(string playerName, int score)
 		{
 			Debug.Log("OnScoreIncrease");
 			LastSavedPlayerName = playerName;
 			CurrentState.OnScoreIncrease(this, playerName, score);
 		}
 
-		private void Update()
-		{
-			CurrentState.OnUpdate(this);
-
-			if (CurrentState is GameplayState)
-			{
-				Registry();
-			}
-		}
-
-		private void Registry()
-		{
-			foreach (PlayerScore playerScore in PlayerScores)
-			{
-				playerScore.ScoreIncrease += OnScoreIncrease;
-			}
-		}
+		private void Update() => CurrentState.OnUpdate(this);
 	}
 }
