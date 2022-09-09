797b7d72a194a41ed77518b36c21e029364ac70f
Rapuška <megarubka44@gmail.com>
2022-09-09 13:47:13 +0300

    Fixed

--- Assets/Code/UI/FullscreenToggle.cs
+++ Assets/Code/UI/FullscreenToggle.cs
@@ -7,68 +7,33 @@ namespace Code.UI
 	public class FullscreenToggle : MonoBehaviour
 	{
 		[SerializeField] private Toggle _toggle;
-		[SerializeField] private Vector2Int _minimalResolutionSize = new Vector2Int(700, 394);
+		[SerializeField] private int _windowedResolutionWidth = 700;
+		[SerializeField] private int _windowedResolutionHeight = 394;
 
-		private Resolution _cashedWindowResolution;
-		private Resolution _minimalWindowResolution;
-		private bool _toFullscreen;
+		private void Start() => _toggle.isOn = Screen.fullScreen;
 
-		private bool IsFullScreen
+		private void OnEnable() => _toggle.onValueChanged.AddListener(ToggleFullscreen);
+
+		private void OnDisable() => _toggle.onValueChanged.RemoveListener(ToggleFullscreen);
+
+		private void ToggleFullscreen(bool isToFullscreen)
+			=> this.Do
+			(
+				@if: isToFullscreen,
+				@true: (_) => ToFullscreen(),
+				@false: (_) => ToWindowed()
+			);
+
+		private static void ToFullscreen()
 		{
-			set => Screen.fullScreenMode = value
-				? FullScreenMode.FullScreenWindow
-				: FullScreenMode.Windowed;
+			const FullScreenMode fullScreenMode = FullScreenMode.ExclusiveFullScreen;
+
+			Screen.fullScreenMode = fullScreenMode;
+			Resolution currentResolution = Screen.currentResolution;
+			Screen.SetResolution(currentResolution.width, currentResolution.height, fullScreenMode);
 		}
 
-		private void Start()
-		{
-			_toggle.isOn = Screen.fullScreen;
-			_minimalWindowResolution = ResolutionExtensions.New(_minimalResolutionSize);
-			_cashedWindowResolution = _minimalWindowResolution;
-
-			_toggle.onValueChanged.AddListener(ToggleFullscreen);
-		}
-
-		private void OnDestroy()
-		{
-			_toggle.onValueChanged.RemoveListener(ToggleFullscreen);
-		}
-
-		private void Update()
-		{
-			if (UnityEngine.Input.GetKeyDown(KeyCode.F11) == false)
-			{
-				return;
-			}
-
-			_toggle.isOn = Screen.fullScreen;
-			ToggleFullscreen(Screen.fullScreen == false);
-		}
-
-		private void ToggleFullscreen(bool toFullscreen)
-		{
-			_toFullscreen = toFullscreen;
-			ActualizeScreenMode();
-		}
-
-		private void ActualizeScreenMode()
-		{
-			Resolution targetResolution = GetTargetResolution(_toFullscreen);
-
-			Screen.SetResolution(targetResolution.width, targetResolution.height, _toFullscreen);
-		}
-
-		private Resolution GetTargetResolution(bool toFullscreen)
-			=> toFullscreen
-				? ToFullscreen()
-				: ToWindowed();
-
-		private Resolution ToFullscreen()
-		{
-			_cashedWindowResolution = ResolutionExtensions.WindowResolution;
-			return ResolutionExtensions.ScreenResolution;
-		}
-
-		private Resolution ToWindowed() => ResolutionExtensions.Max(_cashedWindowResolution, _minimalWindowResolution);
+		private void ToWindowed()
+			=> Screen.SetResolution(_windowedResolutionWidth, _windowedResolutionHeight, FullScreenMode.Windowed);
 	}
 }
